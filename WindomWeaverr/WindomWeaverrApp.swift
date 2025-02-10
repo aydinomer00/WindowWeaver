fd0fb8db10d9bd4139d64ee565a80a8e301f0937
aydinomer00 <aydinomermurat28@gmail.com>
2025-02-10 21:39:22 +0300

    Initial Commit

---
+++ WindomWeaverr/WindomWeaverrApp.swift
@@ -0,0 +1,17 @@
+//
+//  WindomWeaverrApp.swift
+//  WindomWeaverr
+//
+//  Created by Ömer Murat Aydın on 10.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct WindomWeaverrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
